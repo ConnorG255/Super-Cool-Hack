878080d79c0231cc2991d8a547b911d4265c7889
conno <25connorgriffin@gmail.com>
2023-12-01 22:19:21 -0800

    temptemplate

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,22 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-white text-3xl'>asdfasdf</h1>
+    <main className="min-h-screen  bg-slate-900 text-center">
+      <div className='min-w-screen bg-red-200 h-min-1/6 flex justify-between'>
+        <div className="text-white text-lg font-bold">Title</div>
+
+
+        <div className="space-x-4">
+        
+          <button className="text-white">About</button>
+          <button className="text-white">Sign up</button>
+        </div>
+        </div>
+
+      <h1 className='text-white text-3xl'>Title </h1>
+
+      <div></div>
+
     </main>
   )
 }
